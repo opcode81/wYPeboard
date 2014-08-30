b0d0e0ed4edecd461ac7adbf4137bd848510c9f0
jain <djain@gmx.net>
2014-08-30 21:01:19 +0200

    working on anti-aliased line drawing

--- aaline.py
+++ aaline.py
@@ -12,18 +12,18 @@ def aaline(surface, colour, pos1, pos2, lineWidth=3):
         print "m = ", m
         if m > 0:
             if m >= 1:
-                offs1 = (-(offset + 1), 0)
+                offs1 = (-(offset), 0)
                 offs2 = (offset, 0)
             else:
-                offs1 = (lineWidth-1, -(offset))
+                offs1 = (lineWidth-2, -(offset))
                 offs2 = (0, offset)
         else:
             if m <= -1:
                 offs1 = (-offset, 0)
-                offs2 = (offset + 1, 0)
+                offs2 = (offset, 0)
             else:
                 offs1 = (0, -offset)
-                offs2 = (lineWidth-1, offset)
+                offs2 = (lineWidth-2, offset)
         print offs1, offs2
         pygame.draw.aaline(surface, colour, (pos1[0]+offs1[0], pos1[1]+offs1[1]), (pos2[0]+offs1[0], pos2[1]+offs1[1]))
         pygame.draw.aaline(surface, colour, (pos1[0]+offs2[0], pos1[1]+offs2[1]), (pos2[0]+offs2[0], pos2[1]+offs2[1]))
@@ -33,6 +33,8 @@ if __name__=='__main__':
     lines = []
     lines.append(((10,10), (90,90)))
     lines.append(((10,90), (90,10)))
+    lines.append(((10,90), (20,10)))
+    lines.append(((10,10), (20,90)))
     lines.append(((10,10), (90,30)))
     lines.append(((10,30), (90,10)))
     for l in lines:
